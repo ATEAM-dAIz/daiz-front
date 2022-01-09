a8e677b84d5f5f063f7855c9aaf46972f913d72d
Jsgithubchannel <jsschannel99@gmail.com>
2022-01-09 14:32:02 +0900

    Feat: add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.scss";
+
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </div>
+    </BrowserRouter>
   );
 }
 
