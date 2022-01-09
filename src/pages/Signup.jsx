685a60c0158bd3fbd3780ae98a708798cf25939a
Jsgithubchannel <jsschannel99@gmail.com>
2022-01-09 14:36:11 +0900

    Feat: add Signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { ReactComponent as Logo } from "../assets/logo.svg";
+import styles from "./Signup.module.scss";
+
+const Signup = () => {
+  return (
+    <div className={styles.container}>
+      <Logo className={styles.logo} />
+      <form className={styles.column} noValidate>
+        <input
+          name="id"
+          type="text"
+          placeholder="아이디"
+          required
+          className="input-account"
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="비밀번호"
+          required
+          className="input-account"
+        />
+        <input
+          name="reEnterPassword"
+          type="password"
+          placeholder="비밀번호 확인"
+          required
+          className="input-account"
+        />
+        <input
+          name="nickname"
+          type="text"
+          placeholder="닉네임"
+          required
+          className="input-account"
+        />
+        <button className={`btn-main ${styles.btn}`}>회원가입</button>
+      </form>
+
+      <div className={styles.linkContainer}>
+        <p className={styles.link}>이미 가입하셨나요??</p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
