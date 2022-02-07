cc23b5325c822a16a2f1826ff2012b74b7f2458f
Jsgithubchannel <jsschannel99@gmail.com>
2022-02-07 13:11:39 +0900

    Feat: edit onChange function in Writing

--- src/pages/Writing/Writing.jsx
+++ src/pages/Writing/Writing.jsx
@@ -2,18 +2,36 @@ import React, { useState } from "react";
 
 import styles from "./Writing.module.scss";
 import Navigation from "../../components/Navigation";
+import { postDiary } from "../../services/AuthService";
 
 const Writing = ({ history }) => {
   const [modal, setModal] = useState(false);
-  const [context, setContext] = useState(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [existingContent, setExistingContent] = useState(false);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await postDiary(title, content);
   };
 
-  const onChange = (e) => {
-    const validInputs = e.target.value;
-    validInputs.length === 0 ? setContext(false) : setContext(true);
+  const changeInput = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "title":
+        setTitle(value);
+        break;
+      case "content":
+        value.length === 0
+          ? setExistingContent(false)
+          : setExistingContent(true);
+        setContent(value);
+        break;
+
+      default:
+    }
   };
 
   return (
@@ -28,48 +46,50 @@ const Writing = ({ history }) => {
             placeholder="제목"
             // required
             className="input-writing--title"
+            onChange={changeInput}
           />
           <hr />
           <div className={styles.wrapper}>
             <h2>오늘 무슨 일이 있었나요?</h2>
             <input
-              name="context"
+              name="content"
               type="text"
               placeholder="내용"
               required
               className="input-writing"
-              onChange={onChange}
+              onChange={changeInput}
             />
           </div>
-          {/* {context && setModal(true)} */}
           <button
             className="btn-writing"
-            onClick={() => context && setModal(true)}
+            onClick={() => existingContent && setModal(true)}
           >
             작성완료
           </button>
+
+          {modal && (
+            <div className={styles.modal}>
+              <p>일기가 분석된 후에는 수정할 수 없어요.</p>
+              <p>다 썼는지 확인해주세요.</p>
+              <div className={styles.btnContainer}>
+                <button
+                  className={styles.btnReturn}
+                  onClick={() => setModal(false)}
+                >
+                  돌아가기
+                </button>
+                <button
+                  className={styles.btnConfirm}
+                  onSubmit={onSubmit}
+                  // onClick={() => history.push("/analysis")}
+                >
+                  확인
+                </button>
+              </div>
+            </div>
+          )}
         </form>
       </div>
-      {modal && (
-        <div className={styles.modal}>
-          <p>일기가 분석된 후에는 수정할 수 없어요.</p>
-          <p>다 썼는지 확인해주세요.</p>
-          <div className={styles.btnContainer}>
-            <button
-              className={styles.btnReturn}
-              onClick={() => setModal(false)}
-            >
-              돌아가기
-            </button>
-            <button
-              className={styles.btnConfirm}
-              onClick={() => history.push("/analysis")}
-            >
-              확인
-            </button>
-          </div>
-        </div>
-      )}
 
       <Navigation />
     </>
