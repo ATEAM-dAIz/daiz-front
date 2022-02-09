358370d0deeec16de1f10e1c74cbe268362caad5
jeryjayjae <hjk0639@pukyong.ac.kr>
2022-02-10 02:14:24 +0900

    Fix: set authorization header

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,18 +1,18 @@
 import axios from "axios";
-import { Cookies } from "react-cookie";
 import { serverURL } from "./ServerConst";
-
-const token = new Cookies().get("csrftoken");
-console.log(token);
 
 export const requestSignup = async (email, name, pw, rePw) => {
   await axios
-    .post(`${serverURL}/signup/`, {
-      email: email,
-      name: name,
-      password1: pw,
-      password2: rePw,
-    })
+    .post(
+      `${serverURL}/signup/`,
+      {
+        email: email,
+        name: name,
+        password1: pw,
+        password2: rePw,
+      },
+      { withCredentials: true }
+    )
     .then((response) => {
       console.log(response.data);
     })
@@ -37,7 +37,11 @@ export const requestLogin = async (email, pw) => {
       },
       { withCredentials: true }
     )
-    .then((response) => response.data)
+    .then((response) => {
+      /// token이 필요한 API 요청 시 header Authorization에 token 담아서 보내기
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`
+      return response.data;
+    })
     .catch((e) => {
       console.log(e.response.data);
       return "이메일 혹은 비밀번호를 확인하세요.";
@@ -48,12 +52,6 @@ export const postDiary = async (title, content) => {
   await axios({
     method: "post",
     url: `${serverURL}/diary/`,
-    xstfCookieName: "csrftoken",
-    xsrfHeaderName: "X-CSRFToken",
-    headers: {
-      "X-CSRFToken": token,
-    },
-
     data: {
       title: title,
       content: content,
