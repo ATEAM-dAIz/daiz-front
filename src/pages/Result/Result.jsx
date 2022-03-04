66468e49969ea5b3ac3de72e0321b0ba4c42d518
Jsgithubchannel <jsschannel99@gmail.com>
2022-03-04 22:07:40 +0900

    Feat: add loading spinner for getting result

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -18,6 +18,7 @@ const Result = ({ location }) => {
     diary_id = localStorage.getItem("diary_id");
   }
 
+  const [loading, setLoading] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [swipeUp, getSwipeUp] = useState(false);
@@ -26,42 +27,48 @@ const Result = ({ location }) => {
 
   useEffect(() => {
     const get = async () => {
+      setLoading(true);
       await getDiaryDetail(refresh_token, diary_id).then((response) => {
         setTitle(response["title"]);
         setContent(response["content"]);
       });
+      setLoading(false);
     };
     get();
   }, [refresh_token, diary_id]);
 
   return (
     <div className={styles.container}>
-      <p className={styles.day}>SUN</p>
-      <p className={styles.date}>2021.11.07</p>
-      <hr />
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.content}>{content}</p>
-      <div className={swipeUp ? styles.extended : styles.commentContainer}>
-        <FontAwesomeIcon
-          icon={swipeUp ? faChevronDown : faChevronUp}
-          className={styles.chevronUp}
-          onClick={() => getSwipeUp(!swipeUp)}
-        />
-        <h1 className={styles.commentTitle}>오늘의 코멘트</h1>
-        <p className={styles.commentContent}>
-          {username}님
-          <br />
-          {/* 아침에 작성할 경우: 오늘 하루도 화이팅! */}
-          오늘 하루도 수고하셨어요! <br />
-          현재 {username}님은 [가족관계] 상황에 놓여져있고, [분노]를 느끼시고
-          있네요.
-          <br />
-          <br />
-          화가 폭발할 것 같을 때는 그 자리를 피하는 것도 좋은 방법이라고
-          생각해요
-        </p>
-      </div>
-
+      {!loading && (
+        <div>
+          <p className={styles.day}>SUN</p>
+          <p className={styles.date}>2021.11.07</p>
+          <hr />
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.content}>{content}</p>
+          <div className={swipeUp ? styles.extended : styles.commentContainer}>
+            <FontAwesomeIcon
+              icon={swipeUp ? faChevronDown : faChevronUp}
+              className={styles.chevronUp}
+              onClick={() => getSwipeUp(!swipeUp)}
+            />
+            <h1 className={styles.commentTitle}>오늘의 코멘트</h1>
+            <p className={styles.commentContent}>
+              {username}님
+              <br />
+              {/* 아침에 작성할 경우: 오늘 하루도 화이팅! */}
+              오늘 하루도 수고하셨어요! <br />
+              현재 {username}님은 [가족관계] 상황에 놓여져있고, [분노]를
+              느끼시고 있네요.
+              <br />
+              <br />
+              화가 폭발할 것 같을 때는 그 자리를 피하는 것도 좋은 방법이라고
+              생각해요
+            </p>
+          </div>
+        </div>
+      )}
+      {loading && <div className={styles.loading}></div>}
       <Navigation />
     </div>
   );
