e18c26a0eeddf9b6a4155cb6c15f8522eb00aa30
Jsgithubchannel <jsschannel99@gmail.com>
2022-04-17 22:41:31 +0900

    Refactor: change Services to TS

--- src/services/DiaryService.ts
+++ src/services/DiaryService.ts
@@ -2,34 +2,36 @@ import axios from "axios";
 import { serverURL } from "./ServerConst";
 import { checkAccessToken } from "./AuthService";
 
-export const postDiary = async (refresh_token, title, content) => {
-  const access_token = await checkAccessToken(refresh_token);
-  await axios
-    .post(
-      `${serverURL}/diary/`,
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "application/json",
-        },
-        title: title,
-        content: content,
-      },
-      (axios.defaults.headers.common[
-        "Authorization"
-      ] = `Bearer ${access_token}`)
-    )
-    .then((response) => response.data)
-    .catch((e) => {
-      console.log(e.response);
-      alert("예기치 못한 에러가 발생했습니다.");
-    });
+export const postDiary = async (
+  refresh_token: string,
+  title: string,
+  content: string
+) => {
+  // const access_token = await checkAccessToken(refresh_token);
+  // try {
+  //   axios.post(
+  //     `${serverURL}/diary/`,
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${access_token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //       title: title,
+  //       content: content,
+  //     },
+  //     (axios.defaults.headers.common[
+  //       "Authorization"
+  //     ] = `Bearer ${access_token}`)
+  //   );
+  // } catch (error: any) {
+  //   console.log(error.response);
+  //   alert("예기치 못한 에러가 발생했습니다.");
+  // }
 };
 
-export const getDiary = async (refresh_token) => {
+export const getDiary = async (refresh_token: string) => {
   const access_token = await checkAccessToken(refresh_token);
-
-  return await axios
+  return axios
     .get(`${serverURL}/diary/`, {
       headers: {
         Authorization: `Bearer ${access_token}`,
@@ -44,9 +46,9 @@ export const getDiary = async (refresh_token) => {
     });
 };
 
-export const getDiaryDetail = async (refresh_token, id) => {
+export const getDiaryDetail = async (refresh_token: string, id: string) => {
   const access_token = await checkAccessToken(refresh_token);
-  return await axios
+  return axios
     .get(`${serverURL}/diary/${id}`, {
       headers: {
         Authorization: `Bearer ${access_token}`,
