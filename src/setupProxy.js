ffc4b5b7ca5b141285d0f4b2e74a122f27ac3666
Jsgithubchannel <jsschannel99@gmail.com>
2022-05-11 12:58:02 +0900

    Test: test netlify deployment

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,38 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("/api", {
+//       target: process.env.REACT_APP_HTTPS_PROXY,
+//       changeOrigin: true,
+//       pathRewrite: {
+//         "^/api": "", // URL ^/api -> 공백 변경
+//       },
+//       router: {
+//         "https://daiz.netlify.app/api": "https://ateam-server.tk",
+//       },
+//       secure: false,
+//     })
+//   );
+// };
+
+var HttpsProxyAgent = require("https-proxy-agent");
+var proxy = require("http-proxy-middleware");
+
+// corporate proxy to connect to via environment variables
+var proxyServer =
+  process.env.REACT_APP_HTTPS_PROXY || process.env.REACT_APP_HTTP_PROXY;
+
+var options = {
+  target: "https://daiz.netlify.app", //Proxy url
+  agent: new HttpsProxyAgent(proxyServer), //The actual corporate proxy sever
+  pathRewrite: {
+    "^/api": "", // URL ^/api -> 공백 변경
+  },
+};
+
+var apiProxy = proxy("/api", options);
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: process.env.REACT_APP_HTTPS_PROXY,
-      changeOrigin: true,
-      pathRewrite: {
-        "^/api": "", // URL ^/api -> 공백 변경
-      },
-      router: {
-        "https://daiz.netlify.app/api": "https://ateam-server.tk",
-      },
-      secure: false,
-    })
-  );
+  app.use("/api/ae", apiProxy);
 };
