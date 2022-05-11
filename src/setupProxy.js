6a19683fb2eab66133f9830c2ddaa93333e3daae
Jsgithubchannel <jsschannel99@gmail.com>
2022-05-11 15:48:34 +0900

    Test: test netlify redirect

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,10 +3,10 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://ateam-server.tk",
+      target: process.env.REACT_APP_HTTPS_PROXY,
       changeOrigin: true,
       pathRewrite: {
-        "^/api": "", // URL ^/api -> 공백 변경
+        "^/\\api": "", // URL ^/api -> 공백 변경
       },
       secure: false,
     })
