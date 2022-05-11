0fc8836b74ec0129773448fe315028c96fcc30f6
Jsgithubchannel <jsschannel99@gmail.com>
2022-05-11 11:31:30 +0900

    Feat: add router in proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,17 @@
-const HttpsProxyAgent = require("https-proxy-agent");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// corporate proxy to connect to
-const proxyServer =
-  process.env.REACT_APP_HTTPS_PROXY || process.env.REACT_APP_HTTP_PROXY;
-
-const options = {
-  target: "https://daiz.netlify.app",
-  agent: new HttpsProxyAgent(proxyServer),
-};
-
-const apiProxy = createProxyMiddleware(options);
-
 module.exports = function (app) {
-  app.use("/api", apiProxy);
+  app.use(
+    createProxyMiddleware("/api", {
+      target: process.env.REACT_APP_HTTPS_PROXY,
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "", // URL ^/api -> 공백 변경
+      },
+      router: {
+        "https://daiz.netlify.app/api": "https://ateam-server.tk",
+      },
+      secure: false,
+    })
+  );
 };
